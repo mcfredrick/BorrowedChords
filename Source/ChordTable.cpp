ac2a9ab6130051081961af5a3d6406cee5a4c577
Matt <49464742+mcfredrick@users.noreply.github.com>
2021-05-11 22:37:05 -0400

    Fix warnings. Remove busy debug log.

--- Source/ChordTable.cpp
+++ Source/ChordTable.cpp
@@ -13,23 +13,23 @@ namespace view
 {
 	void ChordTable::initColumns()
 	{
-		auto& header = getHeader();
+		auto& h = getHeader();
 		auto scale = GetController().GetHomeScale();
 		const auto rootNote = GetController().GetRootNote();
-		int num = scale.intervals.size();
+		int num = int( scale.intervals.size() );
 		int pitch = rootNote.pitch;
 		auto colWidth = std::max( 20, getWidth() / ( num + 1 ) );
 
-		header.removeAllColumns();
+		h.removeAllColumns();
 
-		header.addColumn( "Scale" , 1, colWidth, /*minimumWidth=*/30, /*maximumWidth=*/-1/*, propertyFlags=defaultFlags, insertIndex=-1*/);
+		h.addColumn( "Scale" , 1, colWidth, /*minimumWidth=*/30, /*maximumWidth=*/-1/*, propertyFlags=defaultFlags, insertIndex=-1*/);
 
 		// Column IDs must start at 1.
 		for (int i = 0; i < num; ++i)
 		{
 			Note note({ pitch });
-			DBG(note.GetNoteDescr());
-			header.addColumn(note.GetNoteDescr(), i + 2, colWidth, /*minimumWidth=*/30, /*maximumWidth=*/-1/*, propertyFlags=defaultFlags, insertIndex=-1*/);
+		//	DBG(note.GetNoteDescr());
+			h.addColumn(note.GetNoteDescr(), i + 2, colWidth, /*minimumWidth=*/30, /*maximumWidth=*/-1/*, propertyFlags=defaultFlags, insertIndex=-1*/);
 			pitch += scale.intervals[i];
 		}
 	}
